3375921a4a6b05b58d1fd794884139fdc529dd02
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 15:45:00 +0600

    Jsonweb token setup added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+// SET TOKEN .
+
+app.post('/api/set-token' , (req , res)=> { 
+    const user  = req.body; 
+    const token = jwt.sign(user, process.env.ACCESS_SECRET, { expiresIn: '12h' })
+    res.send({ token })
+})
+
+
 
 app.get('/', (req, res) => {
     res.send('SOUND SAFARI SERVER IS RUNNING');
