d250b025e463c5a4822b99255781297b11392fa8
Hasib Hossain <boraborhasib@gmail.com>
2023-06-07 18:49:35 +0600

    index js updated

--- index.js
+++ index.js
@@ -11,13 +11,66 @@ app.use(express.json());
 
 // Routes
 // SET TOKEN .
-
-app.post('/api/set-token' , (req , res)=> { 
-    const user  = req.body; 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorize access' })
+    }
+    const token = authorization?.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ error: true, message: 'forbidden user or token has expired' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+app.post('/api/set-token', (req, res) => {
+    const user = req.body;
     const token = jwt.sign(user, process.env.ACCESS_SECRET, { expiresIn: '12h' })
     res.send({ token })
 })
 
+// MONGO DB ROUTES
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgfriso.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        const database = client.db("sound_safari");
+        const userCollection = database.collection("users");
+
+        app.post('/new-user', verifyJWT, async (req, res) => {
+            const newUser = req.body;
+            const email = newUser.email;
+            if (email !== req.decoded.email) {
+                return res.status(401).send({ error: true, message: 'Unauthorize access' });
+            }
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -26,6 +79,6 @@ app.get('/', (req, res) => {
 
 
 // Listen
-app.listen(port ,()=> { 
+app.listen(port, () => {
     console.log(`SERVER IS RUNNING ON PORT ${port}`);
 })
