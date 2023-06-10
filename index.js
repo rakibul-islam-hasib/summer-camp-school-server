196121644cf0b0615957e716710883af4bdda719
Hasib Hossain <boraborhasib@gmail.com>
2023-06-10 21:30:55 +0600

    Payment intent Updated

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ async function run() {
 
 
         // PAYMENT ROUTES
-        app.post('/create-payment-intent', async (req, res) => {
+        app.post('/create-payment-intent', verifyJWT ,  async (req, res) => {
             const { price } = req.body;
             const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
