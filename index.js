a0c24e8a59cd47830fe8eb339146ee12ac07ce88
Hasib Hossain <boraborhasib@gmail.com>
2023-06-24 15:08:11 +0600

    New updated

--- index.js
+++ index.js
@@ -459,7 +459,11 @@ async function run() {
             const result = await appliedCollection.insertOne(data);
             res.send(result);
         })
-
+        app.get('/applied-instructors/:email',   async (req, res) => {
+            const email = req.params.email;
+            const result = await appliedCollection.findOne({email});
+            res.send(result);
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
